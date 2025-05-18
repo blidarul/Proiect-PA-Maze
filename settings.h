8816df3c6e64e5ee5dd60ef211e1a1ab302ba0c5
andreirares5 <raresandrei523@yahoo.ro>
2025-05-18 22:30:40 +0300

    add settings

---
+++ settings.h
@@ -0,0 +1,28 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <stdbool.h>
+#include "raylib.h"
+
+typedef struct 
+{
+    float volume;          
+    float sensitivity;     
+    int character_size;         
+
+    Rectangle volume_slider;      
+    Rectangle sensitivity_slider;
+    Rectangle charsize_slider;
+
+    bool volume_dragging;
+    bool sensitivity_dragging;
+    bool charsize_dragging;
+} Settings;
+
+void initialize_settings(Settings *settings, int screen_width, int screen_height);
+
+void update_settings(Settings *settings);
+
+void draw_settings(const Settings *settings);
+
+#endif
